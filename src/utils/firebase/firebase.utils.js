18fa28e674aaa77969380f2ddddc4656480e071c
Jalil M. López <jalilmanuel25@gmail.com>
2022-09-13 11:28:12 -0500

    Users documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDjH_fGO6VDwkSp8RutLiWyFf5Q_GOFbwY",
     authDomain: "crwn-clothing-db-ac1e3.firebaseapp.com",
@@ -25,3 +32,11 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+}
